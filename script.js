c2084ec7634e80d175ac0c11c6c8f266d718999b
Olumide2596 <olumide2596@gmail.com>
2022-09-07 19:29:24 +0100

    commit

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 'use strict';
 
 const questions = document.querySelectorAll('.qtn');
-questions.forEach(qtn => {
-	qtn.addEventListener('click', function () {
-		qtn.nextElementSibling.classList.toggle('hidden');
+const answers = document.querySelectorAll('.answer');
+const subImage = document.querySelector('.sub-image');
 
+questions.forEach(qtn => {
+	const reset = function () {
+		qtn.nextElementSibling.classList.add('hidden');
+
+		qtn.firstElementChild.classList.remove('bold');
+		qtn.querySelector('.img').style.transform = 'rotate(0deg)';
+		if (window.innerWidth < 600) subImage.style.left = '0%';
+		else subImage.style.left = '-25%';
+	};
+
+	qtn.addEventListener('click', function () {
 		if (qtn.nextElementSibling.classList.contains('hidden')) {
-			qtn.firstElementChild.classList.remove('bold');
-			qtn.querySelector('.img').style.transform = 'rotate(0deg)';
-		} else {
+			qtn.nextElementSibling.classList.remove('hidden');
 			qtn.firstElementChild.classList.add('bold');
 			qtn.querySelector('.img').style.transform = 'rotate(180deg)';
+			if (window.innerWidth < 600) subImage.style.left = '10%';
+			else subImage.style.left = '-35%';
+		} else {
+			reset();
 		}
 	});
 });
+
+console.log(window.innerWidth);
+console.log(answers);
